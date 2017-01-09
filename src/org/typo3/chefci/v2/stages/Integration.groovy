be2e0ab2606ad552cebc6e1fc6b3a9dc7dea0f84
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-09 07:57:15 +0100

    Remove superfluous closure

--- src/org/typo3/chefci/v2/stages/Integration.groovy
+++ src/org/typo3/chefci/v2/stages/Integration.groovy
@@ -15,15 +15,13 @@ class Integration extends AbstractStage {
 
     private def testkitchen(){
         script.node {
-            script.script {
-                wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "XTerm"]) {
-                    result = script.sh(script: 'kitchen test --destroy always', returnStatus: true)
-                    if (result != 0) {
-                        script.echo "kitchen returned non-zero exit status"
-//                        echo "Archiving test-kitchen logs"
-//                        archive(includes: ".kitchen/logs/${instanceName}.log")
-                        script.error("kitchen returned non-zero exit status")
-                    }
+            wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "XTerm"]) {
+                result = script.sh(script: 'kitchen test --destroy always', returnStatus: true)
+                if (result != 0) {
+                    script.echo "kitchen returned non-zero exit status"
+//                    echo "Archiving test-kitchen logs"
+//                    archive(includes: ".kitchen/logs/${instanceName}.log")
+                    script.error("kitchen returned non-zero exit status")
                 }
             }
         }
