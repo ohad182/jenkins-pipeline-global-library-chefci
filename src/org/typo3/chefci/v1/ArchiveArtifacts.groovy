20c2efbb12d73e6ebdddf22bb24e560dfe590a74
ohad182 <ohad182@gmail.com>
2017-01-12 16:10:18 +0200

    Update ArchiveArtifacts.groovy

--- src/org/typo3/chefci/v1/ArchiveArtifacts.groovy
+++ src/org/typo3/chefci/v1/ArchiveArtifacts.groovy
@@ -3,7 +3,7 @@
 package org.typo3.chefci.v1;
 
 def archiveArtifacts(){
-    def artifactName = "cookbooks-${env.BUILD_TAG}.tar.gz"
+    //def artifactName = "cookbooks-${env.BUILD_TAG}.tar.gz"
     echo "Skipping archive step, as long as we have private data in some cookbooks... :-/"
     // sh("berks package ${artifactName}")
     // archive(includes: artifactName)
