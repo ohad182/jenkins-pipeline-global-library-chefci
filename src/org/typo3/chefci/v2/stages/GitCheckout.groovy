47c05a7933692ac25cf25f55bf94f8075c97165b
ohad182 <ohad182@gmail.com>
2017-01-15 16:39:23 +0200

    Update GitCheckout.groovy

--- src/org/typo3/chefci/v2/stages/GitCheckout.groovy
+++ src/org/typo3/chefci/v2/stages/GitCheckout.groovy
@@ -1,19 +1,20 @@
 package org.typo3.chefci.v2.stages
 
 class GitCheckout implements Stage {
-
+    StageInfo info;
+    
     GitCheckout(Object script, String stageName) {
-        super(script, stageName)
+        info = new StageInfo(script, stageName)
     }
 
     @Override
     void execute() {
-        script.stage(stageName) {
-            script.node {
+        info.script.stage(stageName) {
+            info.script.node {
                 //script.checkout(script.scm)
                 // we e.g. have a .kitchen.docker.yml left from the last run. Remove that.
                 //script.sh("git clean -fdx")
-                script.echo "git checkout"
+                info.script.echo "git checkout"
             }
         }
     }
