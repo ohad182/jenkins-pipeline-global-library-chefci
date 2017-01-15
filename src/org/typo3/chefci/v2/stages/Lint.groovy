82ef393b3e6bce074c8dd1626d159fca0e2cdb7c
ohad182 <ohad182@gmail.com>
2017-01-15 16:40:40 +0200

    Update Lint.groovy

--- src/org/typo3/chefci/v2/stages/Lint.groovy
+++ src/org/typo3/chefci/v2/stages/Lint.groovy
@@ -1,29 +1,30 @@
 package org.typo3.chefci.v2.stages
 
 class Lint implements Stage {
-
+    StageInfo info;
+    
     Lint(Object script, String stageName) {
-        super(script, stageName)
+        info = new StageInfo(script, stageName)
     }
 
     @Override
     void execute() {
-        script.stage(stageName) {
+        info.script.stage(stageName) {
             foodcritic()
             rubocop()
         }
     }
 
     private def foodcritic(){
-        script.node {
-            script.echo('foodcritic .')
+        info.script.node {
+            info.script.echo('foodcritic .')
         }
     }
 
     private def rubocop(){
-        script.node {
+        info.script.node {
             // see also http://atomic-penguin.github.io/blog/2014/04/29/stupid-jenkins-and-chef-tricks-part-1-rubocop/
-            script.echo('rubocop')
+            info.script.echo('rubocop')
             //script.step([$class: 'WarningsPublisher', canComputeNew: false, canResolveRelativePaths: false, consoleParsers: [[parserName: 'Foodcritic'], [parserName: 'Rubocop']], defaultEncoding: '', excludePattern: '', healthy: '', includePattern: '', unHealthy: ''])
             //script.step([$class: 'AnalysisPublisher'])
         }
