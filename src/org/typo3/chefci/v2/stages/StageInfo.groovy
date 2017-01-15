6496c2041e3657382ae50984a5905634d0802ec9
ohad182 <ohad182@gmail.com>
2017-01-15 16:23:03 +0200

    Update and rename AbstractStage.groovy to StageInfo.groovy

--- src/org/typo3/chefci/v2/stages/StageInfo.groovy
+++ src/org/typo3/chefci/v2/stages/StageInfo.groovy
@@ -1,15 +1,12 @@
 package org.typo3.chefci.v2.stages
 
-abstract class AbstractStage implements Stage {
+abstract class StageInfo extends Serializable {
 
     def stageName
     def script
 
-    AbstractStage(script, String stageName) {
+    StageInfo(script, String stageName) {
         this.script = script
         this.stageName = stageName
     }
-
-    abstract void execute()
-
 }
