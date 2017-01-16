7105fdf49bdc26fcef30f1afed5ea2e8321e2413
ohad182 <ohad182@gmail.com>
2017-01-16 17:28:24 +0200

    Update Pipeline.groovy

--- src/org/typo3/chefci/v2/Pipeline.groovy
+++ src/org/typo3/chefci/v2/Pipeline.groovy
@@ -20,7 +20,7 @@ class Pipeline implements Serializable {
  //           envs(FOO: 'bar', TEST: '123')
  //           propertiesFile('env.properties')
         }
-        steps.echo "chaim - ${script.environment.chaim}"
+        steps.echo "chaim - ${env.chaim}"
     
         return new Builder(script, steps)
     }
